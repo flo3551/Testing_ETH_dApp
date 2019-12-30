fad3ae4fbe61dc2ebe444069ed1c9d7708493649
unknown <fdealmeida@inflexsys.com>
2019-12-30 11:49:42 +0100

    setup client side application, blockchain connection and retrieving data

--- contracts/Files.sol
+++ contracts/Files.sol
@@ -2,5 +2,21 @@ pragma solidity ^0.5.11;
 
 contract Files {
     uint public filesCount = 0;
-    uint public id;
+
+    struct File {
+        uint id;
+        string base64content;
+        bool isPrivate;
+    }
+
+    mapping (uint => File) public files;
+
+    constructor() public {
+        createFile("I'm the first sample file");
+    }
+
+    function createFile(string memory _content) public {
+        filesCount ++;
+        files[filesCount] = File(filesCount, _content, false);
+    }
 }
